b0644f1e1e7da43809fa4aa21c5d4d15d1755ac6
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2021-09-22 14:42:56 +0200

    Expanded edges function

--- src/Language/MicroC/ProgramGraph.hs
+++ src/Language/MicroC/ProgramGraph.hs
@@ -1,57 +1,85 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module Language.MicroC.ProgramGraph where
 
-import qualified Language.MicroC.AST as AST
 import qualified Control.Monad.State.Lazy as S
-import Control.Lens
+import qualified Language.MicroC.AST      as AST
 
 type State = Int
 
-data Action 
+data Action
   = DeclAction AST.Declaration
   | AssignAction (AST.LValue 'AST.CInt) (AST.RValue 'AST.CInt)
   | ReadAction (AST.LValue 'AST.CInt)
   | WriteAction (AST.RValue 'AST.CInt)
   | BoolAction (AST.RValue 'AST.CBool)
+    deriving (Show)
 
 type Edge = (State, Action, State)
 
 type PG = [Edge]
 
-instance Show Action where
-  show (DeclAction ident) = "int " ++ ident ++ "\n" -- TODO: Finish the pretty printer
+class Monad m => MonadNode m where
+  newState :: m State
 
-data Memory = Memory
-  { _nextFree :: State
-  }
+instance MonadNode (S.State Int) where
+  newState = do
+    c <- S.get
+    S.modify (+1)
+    return c
 
-makeLenses ''Memory
+toPG :: AST.Program -> PG
+toPG p = S.evalState (toPG' 0 (-1) p) (1 :: Int)
 
-type Comp a = S.State Memory a
+toPG' :: MonadNode m => State -> State ->  AST.Program -> m PG
+toPG' qs qe (AST.Program ds ss) = do
+  q <- newState
+  decs <- decsToPG qs q ds
+  stats <- statsToPG q qe ss
+  return $ decs <> stats
 
-toPG :: State -> State ->  AST.Program -> Comp PG
-toPG qs qe (AST.Program ds ss) = do
-  decs <- decsToPG qs qe ds
-  stats <- statsToPG qe ss
-  return $ concat $ decs ++ stats
-
-decsToPG :: State -> State -> [AST.Declaration] -> Comp PG
-decsToPG qs qe [] = return []
+decsToPG :: MonadNode m => State -> State -> [AST.Declaration] -> m PG
+decsToPG _ _ [] = return []
+decsToPG qs qe [x] = do
+  d <- decToPG qs qe x
+  return [d]
 decsToPG qs qe (d : ds) = do
-  counter <- S.gets counter
-  let q = "s_" ++ show counter
-  counter += 1
-  d1 <- decToPG qs q d
-  rest <- decsToPG q qe ds
-  return $ d1 ++ rest
-  
-decToPG :: State -> State -> AST.Declaration -> Comp PG
-decToPG qs qe d = do
-  end .= qe
-  return [(qs, DeclAction d, qe)]
+  q <- newState
+  (:) <$> decToPG qs q d <*> decsToPG q qe ds
 
--- stmToPG :: State -> State -> [AST.Statement] -> Comp PG
--- stmToPG qs qe (AST.Write r) = return [(qs, WriteAction r, qe)]
-statsToPG :: State -> State 
-statsToPG = undefined
+decToPG :: MonadNode m => State -> State -> AST.Declaration -> m Edge
+decToPG qs qe d = return (qs, DeclAction d, qe)
+
+statsToPG :: MonadNode m => State -> State -> [AST.Statement] -> m PG
+statsToPG _ _ [] = return []
+statsToPG qs qe [x] = stmToPG qs qe x
+statsToPG qs qe (s : ss) = do
+  q <- newState
+  (++) <$> stmToPG qs q s <*> statsToPG q qe ss
+
+stmToPG :: MonadNode m => State -> State -> AST.Statement -> m PG
+stmToPG qs qe (AST.Write r) = return [(qs, WriteAction r, qe)]
+stmToPG qs qe (AST.Read l) = return [(qs, ReadAction l, qe)]
+stmToPG qs qe (AST.Assignment l r) = return [(qs, AssignAction l r, qe)]
+stmToPG qs qe (AST.RecordAssignment i r1 r2) = do
+  q <- newState
+  return [ (qs, AssignAction (AST.FieldAccess i "fst") r1, q)
+         , (q, AssignAction (AST.FieldAccess i "snd") r2, qe) ]
+stmToPG qs qe (AST.IfThen cond body) = do
+  q <- newState
+  let (pos, neg) = branch cond
+  rest <- statsToPG q qe body
+  return $ [(qs, pos, q), (qs, neg, qe)] ++ rest
+stmToPG qs qe (AST.While cond body) = do
+  q <- newState
+  let (pos, neg) = branch cond
+  rest <- statsToPG q qs body
+  return $ [(qs, pos, q), (qs, neg, qe)] ++ rest
+
+-- TODO: Implement [if - then - else] and remove this line:
+stmToPG _ _ _ = return []
+
+branch :: AST.RValue 'AST.CBool -> (Action, Action)
+branch v = (BoolAction v, BoolAction (AST.Not v))
