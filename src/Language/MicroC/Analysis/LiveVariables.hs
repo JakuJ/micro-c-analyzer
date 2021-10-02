870b19e6278050213a0535406d1dc3a7d6c9cbfd
iyyel <i@iyyel.io>
2021-10-03 00:27:28 +0200

    LV: Refined gen function

--- src/Language/MicroC/Analysis/LiveVariables.hs
+++ src/Language/MicroC/Analysis/LiveVariables.hs
@@ -8,9 +8,9 @@ module Language.MicroC.Analysis.LiveVariables
 ) where
 
 import qualified Data.Set                     as S
+import           Language.MicroC.AST          hiding (Variable)
+import qualified Language.MicroC.AST          as AST
 import           Language.MicroC.Analysis
-import           Language.MicroC.AST hiding ( Variable )
-import qualified Language.MicroC.AST as AST
 import           Language.MicroC.ProgramGraph
 
 -- | A result of a Live Variables analysis.
@@ -33,21 +33,26 @@ instance Analysis LV where
   stateOrder = backward
   -- TODO: Kill and gen functions
   kill _ = S.empty
-  gen (_, DeclAction _, _) = S.empty 
-  gen (_, AssignAction _ r, _) = fv r
-  gen (_, ReadAction _, _) = S.empty 
-  gen (_, WriteAction r, _) = fv r
-  gen (_, BoolAction r, _) = fv r
+  gen (_, action, _) = case action of
+    DeclAction _       -> S.empty
+    AssignAction lv rv -> fv'' lv `S.union` fv rv
+    ReadAction _       -> S.empty
+    WriteAction rv     -> fv rv
+    BoolAction rv      -> fv rv
 
 fv :: RValue a -> S.Set LVResult
-fv (Reference l) = fv' l
-fv (OpA x _ y) = fv x `S.union` fv y
-fv (OpR x _ y) = fv x `S.union` fv y
-fv (OpB x _ y) = fv x `S.union` fv y
-fv (Not r) = fv r
-fv _ = S.empty
+fv (Reference lv)  = fv' lv
+fv (OpA rva _ rvb) = fv rva `S.union` fv rvb
+fv (OpR rva _ rvb) = fv rva `S.union` fv rvb
+fv (OpB rva _ rvb) = fv rva `S.union` fv rvb
+fv (Not rv)        = fv rv
+fv _               = S.empty
 
 fv' :: LValue a -> S.Set LVResult
-fv' (AST.Variable i) = S.singleton (Variable i)
-fv' (ArrayIndex i r) = S.singleton (Array i) `S.union` fv r
+fv' (AST.Variable i)   = S.singleton (Variable i)
+fv' (ArrayIndex i rv)  = S.singleton (Array i) `S.union` fv rv
 fv' (FieldAccess i i') = S.singleton (RecordField i i')
+
+fv'' :: LValue a -> S.Set LVResult
+fv'' (ArrayIndex i rv) = S.singleton (Array i) `S.union` fv rv
+fv'' _                 = S.empty
