70b35183bf21cebfe8a48d5be1b95fa4ec5a847a
iyyel <i@iyyel.io>
2021-10-10 17:18:41 +0200

    Further dev on DangerousVars

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import           Control.Monad.IO.Class                  (MonadIO (..))
 import           Data.Foldable                           (toList)
 import qualified Data.Map                                as M
 import           Language.MicroC.Analysis.FaintVariables (FV)
+import           Language.MicroC.Analysis.DangerousVariables (DV)
 import           Language.MicroC.Interpreter             (MonadEval (..),
                                                           evalProgram)
 import           Language.MicroC.Parser                  (parseProgram)
@@ -25,7 +26,7 @@ analyseFile path = do
     Left err  -> putStrLn $ "ERROR :: " <> err
     Right ast -> do
       let pg = toPG ast
-          solution = roundRobin @FV pg (-1)
+          solution = roundRobin @DV pg (-1)
       putStrLn "AST:"
       print ast
       putStrLn "PG:"
