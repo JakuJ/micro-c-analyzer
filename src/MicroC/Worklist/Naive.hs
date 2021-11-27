6e59a0eef9568142211306fdffeea4cd7c818883
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2021-11-27 14:52:17 +0100

    Naive round-robin uses a "changed" variable

--- src/MicroC/Worklist/Naive.hs
+++ src/MicroC/Worklist/Naive.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
 
 module MicroC.Worklist.Naive
 ( naiveIterative
@@ -14,42 +15,52 @@ import           MicroC.Analysis
 import           MicroC.ProgramGraph
 import           MicroC.Worklist
 
+data Memory m = Memory
+  { _changed :: Bool
+  , _output  :: Assignment m
+  }
+
+makeLenses ''Memory
+
 -- | An implementation of the Round Robin worklist algorithm.
 naiveIterative :: forall m. WorklistAlgorithm m
 naiveIterative pg = Solution mem (-1) -- We do not care about iteration counting
   where
     mem :: Assignment m
-    mem = execState go M.empty
+    mem = execState go (Memory False M.empty) ^. output
 
-    go :: State (Assignment m) ()
+    go :: State (Memory m) ()
     go = do
       -- all states except the first one
       let s0 = if direction @m == Forward then 0 else -1
           qq = allStates pg S.\\ S.singleton s0
 
       -- set bottom value to all states
-      forM_ qq $ \q -> at q ?= bottom
+      forM_ qq $ \q -> output . at q ?= bottom
 
       -- set initial value to state s0
-      at s0 ?= initialValue @m pg
+      output . at s0 ?= initialValue @m pg
 
-      -- iterate until False is returned for every element
-      whileM $ anyM pg $ \e -> do
-        -- get order of states (reversed for backward problems)
-        let (q, q') = stateOrder @m e
-        -- get current solution for q and q'
-        aq <- gets (M.! q)
-        aq' <- gets (M.! q')
-        -- calculate left side of the constraint
-        let leftSide = analyze @m pg e aq
-            satisfied = leftSide `order` aq'
-        if not satisfied then do
-          -- if not satisfied, we set update the solution for state q'
-          at q' ?= supremum aq' leftSide
-          -- and indicate something has changed
-          pure True
-        else
-          pure False
+      -- iterate until 'changed' is no longer True
+      let loop = do
+            forM_ pg $ \e -> do
+              -- get order of states (reversed for backward problems)
+              let (q, q') = stateOrder @m e
+              -- get current solution for q and q'
+              aq <- uses output (M.! q)
+              aq' <- uses output (M.! q')
+              -- calculate left side of the constraint
+              let leftSide = analyze @m pg e aq
+                  satisfied = leftSide `order` aq'
+              unless satisfied $ do
+                -- if not satisfied, we set update the solution for state q'
+                output . at q' ?= supremum aq' leftSide
+                -- and indicate something has changed
+                changed .= True
+            c <- changed <<.= False
+            when c loop
+
+      loop
 
 -- HELPER FUNCTIONS
 
@@ -58,13 +69,3 @@ stateOrder :: forall m. Analysis m => Edge -> (StateNum, StateNum)
 stateOrder (qs, _, qe) = case direction @m of
   Forward  -> (qs, qe)
   Backward -> (qe, qs)
-
--- | Check if any element of a list maps to `True` under a monadic predicate.
-anyM :: Monad m => [a] -> (a -> m Bool) -> m Bool
-anyM xs p = or <$> mapM p xs
-
--- | Repeat a monadic computation until it returns `False`.
-whileM :: Monad m => m Bool -> m ()
-whileM expr = do
-  cond <- expr
-  when cond $ whileM expr
